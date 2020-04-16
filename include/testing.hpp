64c872119381cc5bacac3696e8350179cc520ee1
Luca Francesca <lfrancesca@salesforce.com>
2020-04-16 17:36:28 +0100

    Testing for equality

--- include/testing.hpp
+++ include/testing.hpp
@@ -6,7 +6,7 @@ namespace Vec
     template<typename T>
     bool testVector(const T &expected, const T &actual)
     {
-        return !(expected != actual);
+        return (expected == actual);
     }
 
 } // namespace Vec
