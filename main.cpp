f5cd5216dbf509ad8546ad6f1733e7565b577d31
Luca Francesca <lfrancesca@salesforce.com>
2020-04-29 19:46:07 +0100

    Using Catch2

--- main.cpp
+++ main.cpp
@@ -1,50 +1,16 @@
 #include <Dvec.hpp>
-#include <testing.hpp>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch.hpp>
 
 using namespace Vec;
 
-int main()
-{
-    DVec3d v1 = {0.0, 1.6, 2.5}, v2(3.0, 4.7, 1.0),
-           v4(v1), expect1(0.0, 7.5, 2.5), expect2(3.0, 6.3, 3.5),
-           v3 = (v4 * v2), v5 = (v1 + v2), v6 = (v5 - v2),
-           v7, zero_vector, one = {1.0, 1.0, 1.0};
-    v7 += v1;
-    v7 -= v1;
-    DVec3d v8 = v1 /= one;
-    std::cout << "v1 # " << v1 << "\nv2 # " << v2 << std::endl;
-    if (testVector(expect1, v3))
-    {
-        std::cout << "All good " << expect1 << " is " << v3 << std::endl;
-    }
+DVec3d v1 = {0.0, 1.1, 2.5}, v2(3.0, 0.9, 1.5), v3(3.0, 2.0, 4.0), v0(0,0,0);
+DVec3d v4 = {1.0, 1.0, 2.0}, v5(4.0, 2.1, 0.5), v6(4.0, 2.1, 1.0);
 
-    if (testVector(expect2, v5))
-    {
-        std::cout << "All good " << expect2 << " is " << v5 << std::endl;
-    }
-
-    if (testVector(v1, v6))
-    {
-        std::cout << "All good " << v1 << " is " << v6 << std::endl;
-    }
-
-    if (testVector(zero_vector, v7))
-    {
-        std::cout << "All good " << zero_vector << " is " << v7 << std::endl;
-    }
-
-    if (testVector(v1, v8))
-    {
-        std::cout << "All good " << v1 << " is " << v8 << std::endl;
-    }
-    try
-    {
-        std::cout << v8[4] << std::endl;
-    }
-    catch (const char *msg)
-    {
-        std::clog << "Expected error: " <<  msg << std::endl;
-    }
-
-    return 0;
+TEST_CASE( "Vectors are tested", "[DVec3d]" ) {
+    REQUIRE( v1+v2 == v3 );
+    REQUIRE( v1-v1 == v0 );
+    REQUIRE( v4*v5 == v6 );
 }
+
+
